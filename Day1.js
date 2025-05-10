f2ab2711c387b28f96bf06aeb6b05758bba68491
jayesx4708 <raijayesh2005@gmail.com>
2025-05-10 18:41:25 +0530

    first commit

---
+++ Day1.js
@@ -0,0 +1,7 @@
+"use strict"
+
+
+var num1=34
+var num2=56
+
+console.log(num1+num2);
---
+++ Day2.js
@@ -0,0 +1,27 @@
+
+function jayesh(){
+
+    console.log("Welcome to My First Function")
+    var a=18
+    var b=5
+
+    console.log("sum of a+b= " +  (a+b))
+
+    // window.alert("This is an alert message")
+
+    // var ans= window.confirm("Are you okay")
+    // console.log(ans);
+
+    window.prompt("Do you know my name")
+    console.log("jayesh");
+    
+
+}
+function jay(){
+
+    console.log(" My Function")
+}
+
+
+jay()
+jayesh()
