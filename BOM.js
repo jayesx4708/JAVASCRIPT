760dcee92a429c757a58d56e893413b0285347ad
jayesx4708 <raijayesh2005@gmail.com>
2025-05-12 18:16:27 +0530

    updated

---
+++ BOM.js
@@ -0,0 +1,6 @@
+
+
+
+//  <!-- bom= interaction wd the browser window (browser object model) -->
+//  <!--alert prompt confirm  -->
+//  <!-- alert only show msg wd ok-->
--- String.js
+++ String.js
@@ -10,13 +10,47 @@
 // trim()   ------> Removes the leading and trailing white space and line terminator
 
 
-let txt="    Hello World!     ";//trim() removes whitespace from both sides of a string
-let txt1=txt.trim();
-console.log(txt1)
+// let txt="    Hello World!     ";//trim() removes whitespace from both sides of a string
+// let txt1=txt.trim();
+// console.log(txt1)
 
-// let text="Jayesh" //String
-// console.log(text.length); //ans = 6
+// // let text="Jayesh" //String
+// // console.log(text.length); //ans = 6
 
 
-let text="This isStringfrom Cybrom" //ans = String
-console.log(text.slice(7,13));  
+// let text="This isStringfrom Cybrom" //ans = String
+// console.log(text.slice(7,13));  
+
+
+// ----------length----------
+let a="jayesh";
+console.log(a.length);
+
+// ----------slice----------
+let b="Hello World!";
+let c=b.slice(1,6);
+// slice m last-1 krna hota
+console.log(c);
+
+// ----------concat---------
+let d1="Hello";
+let d2="hiiiii!";
+let d3=d1.concat(" ",d2);
+console.log(d3);
+
+// ----------uppercase---------
+let d="hello world";
+let e=d.toUpperCase();
+console.log(e);
+// ----------lowercase---------
+let f="HELLO WorLD";
+let g=f.toLowerCase();
+console.log(g);
+// ----------substring---------
+let h="Hello World!";
+let i=h.substring(1,6);
+console.log(i);
+// ----------replace---------   
+let j="Hello World!";
+let k=j.replace("World","hehehe");
+console.log(k);
