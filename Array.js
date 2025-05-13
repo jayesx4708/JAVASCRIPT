ba87610324a7fb68fd6a2d925b967beb97689bd9
jayesx4708 <raijayesh2005@gmail.com>
2025-05-13 22:30:55 +0530

    updated

--- Array.js
+++ Array.js
@@ -5,5 +5,9 @@ console.log(okk.split(" "));
 let okk1= "This-Is S-tr-ing"
 console.log(okk1.split("-"));
 
+let okk2= "We-are f-r-o-m c-ybr-om"
+console.log(okk2.split(" "));
+
+
 let okk3= "W-e are f-rom c-ybr-om"
 console.log(okk3.split(" "));
