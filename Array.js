5a3799897149523911214983f9d6475d1a6a0780
jayesx4708 <raijayesh2005@gmail.com>
2025-05-14 19:06:00 +0530

    update

--- Array.js
+++ Array.js
@@ -1,13 +1,6 @@
 // ARRAY KO "[]" SQUARE BRACKET ME LIKHTE HAIN HUMESHA
-let okk= "This Is String"
-console.log(okk.split(" "));
 
-let okk1= "This-Is S-tr-ing"
-console.log(okk1.split("-"));
+let Fruits= ["Apple" , "Mango" , "banana"]
 
-let okk2= "We-are f-r-o-m c-ybr-om"
-console.log(okk2.split(" "));
-
-
-let okk3= "W-e are f-rom c-ybr-om"
-console.log(okk3.split(" "));
+// let fru = new Array(1,2,3,4,5)
+console.log(Fruits);
--- BOM.js
+++ BOM.js
@@ -1,6 +1,9 @@
 
 
 
-//  <!-- bom= interaction wd the browser window (browser object model) -->
-//  <!--alert prompt confirm  -->
+//  <!-- bom = interaction wd the browser window (browser object model) -->
+//  <!-- Methods of BOM -->
+//  <!--alert prompt--->
+//  <!-- confirm--->
+//  <!--alert prompt   -->
 //  <!-- alert only show msg wd ok-->
