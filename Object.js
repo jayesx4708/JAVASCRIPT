8b94e7e54a21de65517620946d0193b4f9a65218
jayesx4708 <raijayesh2005@gmail.com>
2025-05-21 19:28:48 +0530

    update

--- Object.js
+++ Object.js
@@ -7,7 +7,7 @@ for (let i of arr){
 }
 
 // let welcome={
-//     name:"Jayesh",
+//     name:"",
 //     city:"Bhopal",
 //     age:21
 // }
@@ -23,5 +23,35 @@ let welcome={
 }
 for (let i in welcome){
     console.log(i+ "=>" + welcome[i]);
-    
 }
+
+
+
+// -----------------------------------MAP-METHOD--------------------------------------- 
+
+// let num=[3,6,7,9,2,15]
+//    let newnum= num.map( (e)=>{
+//         return e*2
+
+//    })
+//    console.log(newnum);
+   
+// let num=[3,6,7,9,12,15]
+//    let newnum= num.map( (e)=>{
+//         return e*e*e
+
+//    })
+//    console.log(newnum);
+
+// let num=[3,6,7,9,12,15]
+//    let newnum= num.map( (e)=>e*e )
+
+//    console.log(newnum);
+
+// --------------------------------FILTER METHOD---------------------------------------
+let num=[3,6,7,9,12,15]
+   let newnum= num.filter( (e)=>{
+        return e>5
+
+   })
+   console.log(newnum);
