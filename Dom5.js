a4875e21aa440258ac49c0c18315d265b7c328ef
jayesx4708 <raijayesh2005@gmail.com>
2025-06-21 23:17:21 +0530

    update

--- Dom5.js
+++ Dom5.js
@@ -1,14 +1,14 @@
-// ek hi button se text ko show or hide karna
+// // ek hi button se text ko show or hide karna
 
 
-let show=()=>{
-    let text1=document.querySelector(".para")
+// let show=()=>{
+//     let text1=document.querySelector(".para")
 
-    if(text1.style.display=="none")
-       {
-         text1.style.display="block"
-       }
-    else{
-        text1.style.display="none"
-    }    
-}
+//     if(text1.style.display=="none")
+//        {
+//          text1.style.display="block"
+//        }
+//     else{
+//         text1.style.display="none"
+//     }    
+// }
