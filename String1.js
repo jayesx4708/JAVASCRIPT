19e643dc6c6e17279498b20e200a7e75d1734ab7
jayesx4708 <raijayesh2005@gmail.com>
2025-06-21 23:15:21 +0530

    update

--- String1.js
+++ String1.js
@@ -1,13 +1,13 @@
-//anything under single(' ') or double quotes("") is considered as a string
+anything under single(' ') or double quotes("") is considered as a string
 
-// length() ------> Returns the length of the string 
-// slice()  ------> Returns the elements between the two indexes of a string 
-// concat() ------> Combines the text of two strings and returns a new string
-// substring()----> Returns the elements between the two indexes of a string 
-// replace() -----> Used to find a matching text and replaces the text with string 
-// toUpperCase()--> Converts all the alphabets of a string into uppercase 
-// toLowerCase()--> Converts all the alphabets of a string into lowercase 
-// trim()   ------> Removes the leading and trailing white space and line terminator
+length() ------> Returns the length of the string 
+slice()  ------> Returns the elements between the two indexes of a string 
+concat() ------> Combines the text of two strings and returns a new string
+substring()----> Returns the elements between the two indexes of a string 
+replace() -----> Used to find a matching text and replaces the text with string 
+toUpperCase()--> Converts all the alphabets of a string into uppercase 
+toLowerCase()--> Converts all the alphabets of a string into lowercase 
+trim()   ------> Removes the leading and trailing white space and line terminator
 
 // let text="Jayesh" //String
 // console.log(text.length); //ans = 6
