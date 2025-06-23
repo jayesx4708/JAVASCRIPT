a931d7d2e01e18da19eaaeca7219fb121e9d6e68
jayesx4708 <raijayesh2005@gmail.com>
2025-06-23 22:22:30 +0530

    update

--- DOM3.js
+++ DOM3.js
@@ -1,29 +1,29 @@
-// let myname= document.querySelector(".box")
-// let BTN1=document.querySelector(".BTN1") 
-// let BTN2=document.querySelector(".BTN2") 
-// let BTN3=document.querySelector(".BTN3") 
+let myname= document.querySelector(".box")
+let BTN1=document.querySelector(".BTN1") 
+let BTN2=document.querySelector(".BTN2") 
+let BTN3=document.querySelector(".BTN3") 
 
-// //inko globally declare kar diya hai to isko baar baar har function me likhne ki jarurat nahi hai
+//inko globally declare kar diya hai to isko baar baar har function me likhne ki jarurat nahi hai
 
-// let red=()=>{
+let red=()=>{
 
-//     myname.style.border="2px solid black"
-//     // myname.style.backgroundColor="red"
-//     BTN1.style.backgroundColor="red"
-//     myname.style.backgroundImage="url('https://wallpaperaccess.com/full/338397.jpg')"
-// }
+    myname.style.border="2px solid black"
+    // myname.style.backgroundColor="red"
+    BTN1.style.backgroundColor="red"
+    myname.style.backgroundImage="url('https://wallpaperaccess.com/full/338397.jpg')"
+}
 
-// let green=()=>{
+let green=()=>{
     
-//     myname.style.border="2px solid blue"
-//     //myname.style.backgroundColor="green"
-//     BTN2.style.backgroundColor="green"
-//     myname.style.backgroundImage="url('https://images.hdqwalls.com/wallpapers/bmw-m4-artwork-lg.jpg')"
-// }
+    myname.style.border="2px solid blue"
+    //myname.style.backgroundColor="green"
+    BTN2.style.backgroundColor="green"
+    myname.style.backgroundImage="url('https://images.hdqwalls.com/wallpapers/bmw-m4-artwork-lg.jpg')"
+}
 
-// let reset=()=>{
+let reset=()=>{
     
-//     myname.style.border="2px solid black"
-//     myname.style.backgroundColor="white"
-//     BTN3.style.backgroundColor=""
-// }
+    myname.style.border="2px solid black"
+    myname.style.backgroundColor="white"
+    BTN3.style.backgroundColor=""
+}
